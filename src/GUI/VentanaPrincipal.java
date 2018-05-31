b1d18d29a429ad50860a443322111ab944b61e19
Juan Borrajo <juanborrajo10@gmail.com>
2018-05-31 14:04:40 +0200

    Correcion

--- src/GUI/VentanaPrincipal.java
+++ src/GUI/VentanaPrincipal.java
@@ -66,7 +66,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
                 java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Boolean.class
             };
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, true
+                false, false, false, false, false
             };
 
             public Class getColumnClass(int columnIndex) {
